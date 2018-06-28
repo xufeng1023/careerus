27e22017aba2ba21f0d3e7f7ac36463d4948924b
xufeng1023 <xufeng1023@hotmail.com>
2018-06-28 17:47:57 -0400

    'post-page-chinese-title-seo'

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,6 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
-    <title>@yield('title'){{ config('app.name', 'CareerUsa') }}</title>
+    <title>@yield('title'){{ config('app.name', 'CareerUS') }}</title>
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
