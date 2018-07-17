a3ba57431b06c658992a0040b0f2e22f7dad30af
xufeng1023 <xufeng1023@hotmail.com>
2018-07-17 11:48:46 -0400

    'add-cors'

--- resources/views/visaBulletin.blade.php
+++ resources/views/visaBulletin.blade.php
@@ -4,13 +4,6 @@
 {{ '绿卡排期 -' }}
 @endsection
 
-@section('style')
-    <style>
-        .table th, .table td {padding: 0.75rem 7px !important;}
-        h3{font-size:18px !important;}
-    </style>
-@endsection
-
 @section('content')
 <div class="container">
     <div class="row">
