6cb7732863e15ac31779244317c46e6d39480b70
xufeng1023 <xufeng1023@hotmail.com>
2018-07-17 10:57:10 -0400

    'update-iframe-css'

--- resources/views/greenCardFrame.blade.php
+++ resources/views/greenCardFrame.blade.php
@@ -5,7 +5,7 @@ iframe .table .thead-dark th {background-color:#4aba78 !important;}
 .table th, .table td {padding: 0.75rem 7px !important;}
 h3{font-size:18px !important;}
 </style>
-{{ request()->url() }}
+
 @include('_greenCardTable')
 
 </html>
