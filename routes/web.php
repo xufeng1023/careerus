b63a68dc6225f0c73d9b0f96b10a094e9a0e6e1a
xufeng1023 <xufeng1023@hotmail.com>
2018-07-17 11:12:17 -0400

    'add-cors'

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ Auth::routes();
 Route::get('/login/google', '\App\Http\Controllers\Auth\LoginController@redirectToProvider');
 Route::get('/login/google/callback', '\App\Http\Controllers\Auth\LoginController@handleProviderCallback');
 
-Route::get('/green-card', 'GreenCardController@index');
+Route::get('/green-card', 'GreenCardController@index')->middleware('cors');
 Route::get('/绿卡排期', 'GreenCardController@visaBulletin');
 
 Route::get('/', 'PostController@index');
