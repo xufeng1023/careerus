0c14f40fbaa45d70d81254b43ffa3e647316e31a
xufeng1023 <xufeng1023@hotmail.com>
2018-07-17 10:54:21 -0400

    'update-iframe-css'

--- resources/views/greenCardFrame.blade.php
+++ resources/views/greenCardFrame.blade.php
@@ -1,6 +1,7 @@
 <html lang="zh">
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <style>
+iframe .table .thead-dark th {background-color:#4aba78 !important;}
 .table th, .table td {padding: 0.75rem 7px !important;}
 h3{font-size:18px !important;}
 </style>
