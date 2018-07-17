fd9fc0e8a3f38aee2af9d56fd3ca18a160ea7e9a
xufeng1023 <xufeng1023@hotmail.com>
2018-07-17 11:32:38 -0400

    'add-cors'

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => [],
+    'allowedOrigins' => ['https://greencardlegal.com'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
