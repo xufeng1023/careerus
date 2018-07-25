8f0eeabdefb33486877b15822cab0bab21db7bb1
xufeng1023 <xufeng1023@hotmail.com>
2018-07-25 10:12:32 -0400

    'fix-wechat-middleware'

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('api')->get('/search', 'WechatController@search');
+Route::get('/search', 'WechatController@search');
