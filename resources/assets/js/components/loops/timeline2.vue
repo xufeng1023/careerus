684c95cb077aa61e47ba72e39d594a3a0d9a1c42
xufeng1023 <xufeng1023@hotmail.com>
2018-08-30 15:37:05 -0400

    'add-timeline2'

--- resources/assets/js/components/loops/timeline2.vue
+++ resources/assets/js/components/loops/timeline2.vue
@@ -1,6 +1,12 @@
 <template>
-    <div v-if="newDate" class="mx-3 mb-5 text-center border-top border-info" :class="{'w-100':w100}">
-        <div class="d-inline-block px-3 bg-info text-white date-shadow">{{ date }}</div>
+    <div v-if="newDate" class="mx-3 mb-5 d-flex justify-content-center" :class="{'w-100':w100}">
+        <div class="d-flex justify-content-center align-items-center flex-column rotate">
+            <div class="d-flex">
+                <div class="shelf-left"></div>
+                <div class="shelf-right"></div>
+            </div>
+            <div class="px-3 bg-info text-white date-shadow shelf-date">{{ date }}</div>
+        </div>
     </div>
 </template>
 
@@ -22,3 +28,35 @@ export default {
     }
 }
 </script>
+<style>
+.shelf-left {
+    height: 30px;
+    width: 2px;
+    background: #17a2b8;
+    transform: skewX(-27deg) translateX(-22px);
+}
+.shelf-right {
+    height: 25px;
+    width: 3px;
+    background: #17a2b8;
+    transform: skewX(57deg) translate(2px, 0px);
+}
+.shelf-date {
+    transform: skewY(-5deg) translateY(-3px)
+}
+.rotate:hover {
+    cursor: pointer;
+    animation-name: swing;
+    animation-duration: 200ms;
+    animation-iteration-count: 2;
+    animation-timing-function: linear;
+}
+
+@keyframes swing {
+    0%   {transform: rotate(initial);}
+    25% {transform: rotate(-15deg);}
+    50% {transform: rotate(initial);}
+    75% {transform: rotate(15deg);}
+    100% {transform: rotate(initial);}
+}
+</style>
