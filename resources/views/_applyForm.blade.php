71f7191ae99fd2f3404b13d408d0fdba3ab8061d
xufeng1023 <xufeng1023@hotmail.com>
2018-09-05 15:13:08 -0400

    'bigger-arrows'

--- resources/views/_applyForm.blade.php
+++ resources/views/_applyForm.blade.php
@@ -6,15 +6,15 @@
             <div>点击右侧申请</div>
             <div>立即锁定HR内推名额</div>
         </div>
-        <div class="d-none d-lg-flex flex-column justify-content-center h1 text-white m-0">&#8674;</div>
-        <div class="d-flex d-lg-none flex-column justify-content-center h1 text-white m-0">&#8675;</div>
+        <div class="d-none d-lg-flex flex-column justify-content-center text-white m-0 font-weight-bold font-50px">&#8674;</div>
+        <div class="d-flex d-lg-none flex-column justify-content-center text-white m-0 font-weight-bold font-50px">&#8675;</div>
         <div class="d-flex flex-column align-items-center mb-3">
             <img src="{{ asset('images/apply2.png') }}" width="136" alt="">
             <div>系统审核通过</div>
             <div>24小时之后直达HR邮箱</div>
         </div>
-        <div class="d-none d-lg-flex flex-column justify-content-center h1 text-white m-0">&#8674;</div>
-        <div class="d-flex d-lg-none flex-column justify-content-center h1 text-white m-0">&#8675;</div>
+        <div class="d-none d-lg-flex flex-column justify-content-center text-white m-0 font-weight-bold font-50px">&#8674;</div>
+        <div class="d-flex d-lg-none flex-column justify-content-center text-white m-0 font-weight-bold font-50px">&#8675;</div>
         <div class="d-flex flex-column align-items-center">
             <img src="{{ asset('images/apply3.png') }}" width="136" alt="">
             <div>HR内推名额限时限量</div>
