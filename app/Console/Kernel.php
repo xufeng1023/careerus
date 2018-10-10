556dd0303152c455632779f601082f17bf9e4aab
xufeng1023 <xufeng1023@hotmail.com>
2018-10-10 10:59:22 -0400

    'crawl-wechat-again'

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
             $colleges = \DB::connection('dreamgo')->select("
                 SELECT p.post_title FROM wp_posts p INNER JOIN wp_term_relationships tr ON p.ID = tr.object_id WHERE tr.term_taxonomy_id = 3
             ");
-
+            \Log::info($colleges[0]);
             if(count($colleges)) \Cache::forever('dreamgo-collegs', $colleges);
         })->everyMinute();
     }
