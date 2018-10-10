41f00f8516fd1c5ce2ff540afcb588311615ea63
xufeng1023 <xufeng1023@hotmail.com>
2018-10-10 10:40:16 -0400

    'crawl-wechat-again'

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use App\Http\Controllers\BlogController;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call('BlogController@updateCollegesInCache')->everyMinute();
+        $schedule->call('App\Http\Controllers\BlogController@updateCollegesInCache')->everyMinute();
     }
 
     /**
