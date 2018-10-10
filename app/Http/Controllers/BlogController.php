0f484469f61ba85430a765f46874285a10f602af
xufeng1023 <xufeng1023@hotmail.com>
2018-10-10 16:11:37 -0400

    'crawl-wechat-again'

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -37,7 +37,7 @@ class BlogController extends Controller
 
     public function crawlWeChatBlog()
     {
-        $whatToCrawl = ['斯坦福大学'];
+        if(!$whatToCrawl = cache('dreamgo-collegs')) return;
 
         libxml_use_internal_errors(true);
         $meta = '<meta http-equiv="Content-Type" content="text/html; charset=utf8"/>';
@@ -45,8 +45,12 @@ class BlogController extends Controller
         $dom = new \DOMDocument();
         $dom->preserveWhiteSpace = false;
 
-        foreach($whatToCrawl as $query) {
-            $page = file_get_contents('http://weixin.sogou.com/weixin?query='.urlencode($query).'&type=2');
+        foreach($whatToCrawl as $key => $query) {
+            if($key > 0) exit;
+
+            $search = rtrim($query->name, '新闻');
+            
+            $page = file_get_contents('http://weixin.sogou.com/weixin?query='.urlencode($search).'&type=2');
             $page = preg_replace("/[\n\r\t]+/", '', $page);
             preg_match('/<ul class="news-list".*<\/ul>/', $page, $matches);
             $page = preg_replace("/<script>[a-zA-Z0-9.()']*<\/script>/", '', $matches[0]);
@@ -60,8 +64,12 @@ class BlogController extends Controller
                 preg_match_all('/[\x{4e00}-\x{9fff}]+/u', $title, $matches);
                 $title = join('', $matches[0]);
 
-                if(CrawlBlog::whereTitle($title)->exists()) continue;
+                $post = DB::connection('dreamgo')->select("
+                    SELECT ID FROM wp_posts WHERE post_title = '$title' LIMIT 1
+                ");
 
+                if($post) continue;
+                
                 $thumbnail = $li->getElementsByTagName("img")[0]->getAttribute('src');
                 $link = $li->getElementsByTagName('a')[1]->getAttribute('href');
                 $excerpt = $li->getElementsByTagName('p')[0]->textContent;
@@ -79,19 +87,18 @@ class BlogController extends Controller
                 $contentPage = strip_tags($contentPage, '<div><span><pre><p><br><hr><hgroup><h1><h2><h3><h4><h5><h6>
                 <ul><ol><li><dl><dt><dd><strong><em><b><i><u><img><abbr><address>
                 <blockquote><label><caption><table><tbody><td><tfoot><th><thead><tr>');
+   
+                $http = new \GuzzleHttp\Client;
 
-                try {
-                    $crawlBlog = new CrawlBlog;
-                    $crawlBlog->excerpt = $excerpt;
-                    $crawlBlog->title = $title;
-                    $crawlBlog->author = $author;
-                    $crawlBlog->description = $contentPage;
-                    $crawlBlog->thumbnail = $thumbnail;
-                    $crawlBlog->save();
-                } catch (\Illuminate\Database\QueryException $e) {
-                    dd($e->getMessage());
-                    continue;
-                }
+                $http->post('http://18.219.227.57/wp-admin/admin-ajax.php?action=dreamgo_wechat_post', [
+                    'form_params' => [
+                        'title' => $title,
+                        'excerpt' => $excerpt,
+                        'content' => $contentPage,
+                        'category' => $query->term_id,
+                        'thumbnail' => $thumbnail,
+                    ],
+                ]);
             }
         }
     }
@@ -102,7 +109,24 @@ class BlogController extends Controller
             SELECT p.post_title FROM wp_posts p INNER JOIN wp_term_relationships tr ON p.ID = tr.object_id WHERE tr.term_taxonomy_id = 3
         ");
 
-        if(count($colleges)) Cache::forever('dreamgo-collegs', $colleges);
+        if(count($colleges)) {
+
+            $http = new \GuzzleHttp\Client;
+
+            foreach($colleges as $college) {
+                $http->post('http://18.219.227.57/wp-admin/admin-ajax.php?action=dreamgo_update_college_news_category', [
+                    'form_params' => [
+                        'name' => $college->post_title.'新闻'
+                    ],
+                ]);
+            }
+
+            $categories = DB::connection('dreamgo')->select("
+                SELECT term_id, name FROM wp_terms WHERE name LIKE '%新闻'
+            ");
+
+            if(count($categories)) Cache::forever('dreamgo-collegs', $categories);
+        }
     }
 
     public function adminPage()
