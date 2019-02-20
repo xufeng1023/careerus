36f1793237179d36d113af6dff18f014351315fa
xufeng1023 <xufeng1023@hotmail.com>
2019-02-20 12:05:15 -0500

    'update-green-card-page-content-keywords'

--- resources/views/_greenCardTable.blade.php
+++ resources/views/_greenCardTable.blade.php
@@ -3,7 +3,7 @@
 h3{font-size:18px !important;}
 </style>
 <div class="table-responsive">
-<h3 class="text-center">{{ $first->check_at->year }}年{{ $first->check_at->month }}月职业移民绿卡批准日期</h3>
+<h3 class="text-center">{{ $first->check_at->year }}年{{ $first->check_at->month }}月职业移民绿卡排期-批准日期</h3>
 <table class="table table-striped">
     <thead class="thead-dark">
         <tr>
@@ -57,7 +57,7 @@ h3{font-size:18px !important;}
 </div>
 
 <div class="table-responsive">
-<h3 class="text-center">{{ $first->check_at->year }}年{{ $first->check_at->month }}月职业移民绿卡递件日期</h3>
+<h3 class="text-center">{{ $first->check_at->year }}年{{ $first->check_at->month }}月职业移民绿卡排期-递件日期</h3>
 <table class="table table-striped">
     <thead class="thead-dark">
         <tr>
