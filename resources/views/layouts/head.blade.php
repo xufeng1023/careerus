c7608a5b18b458aebd4f38c394c949144d75994a
xufeng1023 <xufeng1023@hotmail.com>
2019-09-06 16:25:05 -0400

    'add-google-verify-meta'

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="WnrFBIFBkSaJHuRIR0wIv00sh7gghlad812tJKknaI8" />
     <meta name="keywords" content="h1b工作,h1b,h1b job,工作内推,hr内推">  
     @yield('description')  
     <link rel="apple-touch-icon" href="{{ asset('images/favicon.png') }}" type="image/png">
