6ac9962106f2917363b2cb9d68ab650d85f3f1ac
xufeng1023 <xufeng1023@hotmail.com>
2019-09-06 16:49:09 -0400

    'add-google-verify-meta'

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="google-site-verification" content="WnrFBIFBkSaJHuRIR0wIv00sh7gghlad812tJKknaI8" />
+
     <meta name="keywords" content="h1b工作,h1b,h1b job,工作内推,hr内推">  
     @yield('description')  
     <link rel="apple-touch-icon" href="{{ asset('images/favicon.png') }}" type="image/png">
     <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png">
     <title>@yield('title'){{ config('app.name', 'CareerUS') }}</title>
-
+    <meta name="google-site-verification" content="WnrFBIFBkSaJHuRIR0wIv00sh7gghlad812tJKknaI8" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110866523-2"></script>
     <script>
